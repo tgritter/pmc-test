e6202bf8e4dd869fedc654c0dfbda246078214b9
Travis Gritter <travis.gritter@pagefreezer.com>
2021-10-14 23:22:08 -0700

    Updates

---
+++ precache-manifest.be845e63e41891ec5fb986caaf5d54e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b10f0e61cfa3c7a2fe09e933093bf83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "399a3fd95c922f000711",
+    "url": "/static/css/main.46860aa0.chunk.css"
+  },
+  {
+    "revision": "93afcabba8095309e861",
+    "url": "/static/js/2.979148c1.chunk.js"
+  },
+  {
+    "revision": "3c21e843167b5281e142e238bb5cf706",
+    "url": "/static/js/2.979148c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399a3fd95c922f000711",
+    "url": "/static/js/main.3c19aeeb.chunk.js"
+  },
+  {
+    "revision": "a0f8e379d1a9ed44ab4f",
+    "url": "/static/js/runtime-main.879ada5e.js"
+  },
+  {
+    "revision": "de8573d561174d3d04c95fbefaa74161",
+    "url": "/static/media/arora_logo.de8573d5.png"
+  },
+  {
+    "revision": "8da8086e4f7a0e605f3b257ec5fefdde",
+    "url": "/static/media/eli_and_ravneet.8da8086e.jpg"
+  },
+  {
+    "revision": "79e101901b0928d91b0ad8e9d3f5f7a0",
+    "url": "/static/media/pmc_logo.79e10190.png"
+  }
+]);
